cddf104c5d3dc49c6e38faa25aee0c916e58ddbe
Sasha <st129690@student.spbu.ru>
2025-04-30 16:36:18 +0300

    Add files via upload

---
+++ Postfix.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+//Задача:
+//Найти сумму элементов на диапозоне от FROM до TO
+
+std::vector<int> Postfix(std::vector<int>  seq, int size) {
+
+	std::vector<int> new_seq( size +1 );
+	new_seq[0] = 0;
+	for (int i = 0; i < size; i++) {
+		new_seq[i + 1] = new_seq[i] + seq[i];
+	}
+	return new_seq;
+}
+
+int Solution(std::vector<int> Postfix, int from, int to) {
+	return Postfix[to] - Postfix[from-1];
+}
+
+
+
+int main() {
+
+	std::vector<int> example = { 1,4,5,6,3,1,54,3 };
+
+	std::vector<int> n_s = Postfix(example, example.size());
+
+	std::cout << Solution(n_s, 2, 3);
+
+
+	return 0;
+
+}
